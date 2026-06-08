13364505b11710708afe7329fafc31272754945d
Bogusz Napierała <bognapierala@gmail.com>
2026-06-08 16:32:03 +0200

    Suspense fix

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import type { FC, ReactNode } from "react";
+import { type FC, type ReactNode, Suspense } from "react";
 
 interface Props {
     children: ReactNode;
 }
 
 export const AuthSessionProvider: FC<Props> = ({ children }) => {
-    return <SessionProvider>{children}</SessionProvider>;
+    return (
+        <Suspense>
+            <SessionProvider>{children}</SessionProvider>
+        </Suspense>
+    );
 };
